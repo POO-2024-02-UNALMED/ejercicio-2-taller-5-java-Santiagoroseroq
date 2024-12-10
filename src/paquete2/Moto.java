2aff3dcb00b0cd0e8df612e13d2a500e9dba3e04
Santiagoroseroq <sroseroq@unal.edu.co>
2024-12-09 23:53:31 -0500

    Update Moto.java

--- src/paquete2/Moto.java
+++ src/paquete2/Moto.java
@@ -7,14 +7,31 @@ public class Moto extends Auto {
     private String modelo;
     public int velocidad = 30;
 
+
     public Moto(String placa,String modelo){
         this.placa = placa;
         this.modelo=modelo;
         this.pitar();
     }
 
+    @Override
+    public void pitar(){
+        System.out.println("Las motos no pitan");
+    }
+
+
     public void acelerar(){
         System.out.println("Avanzo muy rapido");
     }
-    
+    public void arrancar() {
+        System.out.println("Arrancando");
+        }
+    public String getPlaca() {
+            return this.placa;
+        }
+        
+    public String getModelo() {
+            return this.modelo;
+        }
+        
 }
